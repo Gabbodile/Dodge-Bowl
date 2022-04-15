7d4ea7d9734f79a8ae482bcafbc498aabd87b302
Gabbodile <94325069+Gabbodile@users.noreply.github.com>
2022-04-15 11:47:12 +1000

    basic player controller and shooting

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    public int damage = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 5);
+    }
+
+    /*private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Target"))
+        {
+            collision.gameObject.GetComponent<Target>().Hit();
+
+        }
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            collision.gameObject.GetComponent<Enemy>().Hit(damage);
+
+        }
+        Destroy(gameObject);
+    }*/
+
+}
